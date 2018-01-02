81e3ebd9f109da1946e5a0e19ce3a9d2fe4a472b
Michael Centrelli <Centm860@newschool.edu>
2018-01-02 09:30:55 -0500

    Add Redirect for Invalid Credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,14 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:session][:username])
-    if @user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       flash[:notice] = "Successful Login"
       redirect_to root_path
 
     else
       flash[:notice] = "Invalid Credentials"
-      redirect_to new_login_path
+      redirect_to login_path
     end
   end
 end
