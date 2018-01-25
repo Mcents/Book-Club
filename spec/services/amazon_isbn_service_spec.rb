3c26a0431d4e97f025777557b9327960636c1848
Michael Centrelli <centm860@newschool.edu>
2018-01-25 16:55:09 -0500

    Add Test for Amazon Service

---
+++ spec/services/amazon_isbn_service_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe AmazonService do
+	context "#isbn_info" do
+		it "retrieves book info" do
+
+			b_info = AmazonService.new.isbn_info(9780143039945)
+
+			expect(b_info.class).to eq(Hash)
